88eefa1047239adde8b687b972fab629106f9009
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-09-30 18:00:25 +0900

    Change install

--- gh.rb
+++ gh.rb
@@ -19,7 +19,8 @@ class Gh < Formula
     #                       "--disable-silent-rules",
     #                       "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    # system "make", "install" # if this fails, try separate make/make install steps
+    bin.install 'gh'
   end
 
   test do
